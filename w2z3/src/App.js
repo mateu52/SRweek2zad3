0ad351b36ac337f54671641146b69444138870d1
mateu52 <53394614+mateu52@users.noreply.github.com>
2021-08-03 05:06:29 +0200

    czasomierz klasowy i funkcyjny

--- w2z3/src/App.js
+++ w2z3/src/App.js
@@ -2,30 +2,61 @@ import './App.css';
 import React  from "react"; 
 //cd SRweek2zad3\w2z3
 
-class Timex extends React.Component {
+class TimexClass extends React.Component {
   constructor(props){
     super(props);
     this.state={
-      timestart : 0;
+      time:0,
     };
   }
-  render(){
-    const { timestart } = this.state
-    
-    return(
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setTime();
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+  setTime() {
+    this.setState({
+      time: this.state.time + 1,
+    });
+  }
+  render() {
+    return (
       <div>
-        <h1>Hello R</h1>
-        <p>{timer}</p>
+        <h2>Timer in Class Components</h2>
+        <p>Time: {this.state.time} sec</p>
       </div>
-    )
+    );
   }
 }
 
 
+export default function TimexFunction() {
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(time + 1);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, [time]);
+
+  return (
+    <div>
+      <h2>Timer in Functional Components</h2>
+      <p>Time: {time} sec</p>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
-        <Timex />
+        <TimexClass />
+        <TimexFunction />
     </div>
   );
 }
