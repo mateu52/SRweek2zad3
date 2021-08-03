0d80f3555c332a312106018f7b4075166b8d80a2
mateu52 <53394614+mateu52@users.noreply.github.com>
2021-08-03 04:33:21 +0200

    2

--- w2z3/src/App.js
+++ w2z3/src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React  from "react"; 
+//cd SRweek2zad3\w2z3
+
+class Timex extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      timestart : 0;
+    };
+  }
+  render(){
+    const { timestart } = this.state
+    
+    return(
+      <div>
+        <h1>Hello R</h1>
+        <p>{timer}</p>
+      </div>
+    )
+  }
+}
+
 
 function App() {
   return (
     <div>
-        <BlogTile />
+        <Timex />
     </div>
   );
 }
